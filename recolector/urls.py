cc056c1d0adcb7b50d57ecf0b304ed2ca9678599
SantiagoCh94 <santichavez0394@gmail.com>
2023-11-24 09:27:28 -0500

    Se agrega proyecto hasta el crud

---
+++ recolector/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('agregar', views.agregar, name='agregar_recolector'),
+    path('mostrar', views.mostrar, name='mostrar_recolector'),
+    path('ver/<int:pk>', views.editar, name='ver_recolector'),
+    path('eliminar/<int:pk>', views.eliminar, name='eliminar_recolector'),
+
+
+]
---
+++ recolector/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Recolector
+# Create your views here.
+
+
+def agregar(request):
+    if request.method == 'POST':
+
+        cedula = request.POST.get('cedula')
+        nombres = request.POST.get('nombres')
+        apellidos = request.POST.get('apellidos')
+        direccion = request.POST.get('direccion')
+        fecnac = request.POST.get('fecnac')
+        telefono = request.POST.get('telefono')
+
+        miRecolector = Recolector.objects.create(
+            cedula=cedula,
+            nombres=nombres,
+            apellido=apellidos,
+            direccion=direccion,
+            fNacimiento=fecnac,
+            telefono=telefono
+        )
+
+        messages.success(request, 'Registrado correctamente')
+        return redirect('/')
+
+    return render(request, 'recolector/agregar.html', {
+        'title': 'Agregar vendedor',
+    })
+
+
+def mostrar(request):
+    recolectores = Recolector.objects.all()
+    return render(request, 'recolector/mostrar.html', {
+        'title': 'Recolectores',
+        'recolectores': recolectores
+    })
+
+
+def editar(request, pk):
+
+    if request.method == 'POST':
+
+        cedula = request.POST.get('cedula')
+        nombres = request.POST.get('nombres')
+        apellidos = request.POST.get('apellidos')
+        direccion = request.POST.get('direccion')
+        fecnac = request.POST.get('fecnac')
+        telefono = request.POST.get('telefono')
+
+        miRecolector = Recolector.objects.filter(pk=pk).update(
+            cedula=cedula,
+            nombres=nombres,
+            apellido=apellidos,
+            direccion=direccion,
+            fNacimiento=fecnac,
+            telefono=telefono
+        )
+
+        messages.success(request, 'Actualizado correctamente')
+        return redirect('mostrar_recolector')
+
+    recolector = Recolector.objects.get(pk=pk)
+
+    return render(request, 'recolector/editar.html', {
+        'title': 'Recolectores',
+        'recolector': recolector
+    })
+
+
+def eliminar(request, pk):
+    recolector = get_object_or_404(Recolector, pk=pk)
+    recolector.delete()
+    messages.success(request, 'Borrado correctamente')
+    return redirect('mostrar_recolector')
